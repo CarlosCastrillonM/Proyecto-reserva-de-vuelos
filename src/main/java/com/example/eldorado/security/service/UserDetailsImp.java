3b57817a9fa86df8bd4ba43e132476ce3ff0b027
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-10-29 14:35:27 -0500

    fix @Autowired

--- src/main/java/com/example/eldorado/security/service/UserDetailsImp.java
+++ src/main/java/com/example/eldorado/security/service/UserDetailsImp.java
@@ -1,10 +1,14 @@
-package com.example.eldorado.service;
+package com.example.eldorado.security.service;
 
 import com.example.eldorado.entity.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserDetailsImp implements UserDetails {
     private Integer id;
@@ -18,9 +22,41 @@ public class UserDetailsImp implements UserDetails {
 
     private Collection<? extends GrantedAuthority> authorities;
 
-    public UserDetailsImp(Integer id, String username, String email, String password);
+    public UserDetailsImp(Integer id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
 
     public static UserDetailsImp build(User user) {
-        List<GrantedAuthority> authorities = user
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+        return new UserDetailsImp(user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
     }
 }
