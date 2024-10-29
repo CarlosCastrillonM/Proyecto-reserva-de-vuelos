cf4150eaf19b29e2364f01bd6fc0eb94b35816d7
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-10-29 14:34:43 -0500

    new methods

--- src/main/java/com/example/eldorado/repository/UserRepository.java
+++ src/main/java/com/example/eldorado/repository/UserRepository.java
@@ -3,14 +3,14 @@ package com.example.eldorado.repository;
 import com.example.eldorado.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUserName(String name);
     Optional<User> findByEmail(String name);
+    Optional<User> findByUsername(String username);
 
-    Boolean
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 
 }
-
