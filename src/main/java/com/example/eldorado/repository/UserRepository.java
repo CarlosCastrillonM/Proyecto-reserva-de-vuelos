15428070828a74e2c28f7ca4f877089e64da04c5
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-11-24 21:08:23 -0500

    xd

--- src/main/java/com/example/eldorado/repository/UserRepository.java
+++ src/main/java/com/example/eldorado/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String name);
+    Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
 
     Boolean existsByUsername(String username);
