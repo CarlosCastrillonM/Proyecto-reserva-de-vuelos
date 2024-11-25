a53b1623b6ceb63c195756e6b901811684a92e22
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-11-25 01:34:06 -0500

    new declaration of Exceptions

---
+++ src/main/java/com/example/eldorado/exception/ConstantException.java
@@ -0,0 +1,7 @@
+package com.example.eldorado.exception;
+
+public class ConstantException {
+    public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+
+    public static final String INVALID_DATA = "Invalid data";
+}
