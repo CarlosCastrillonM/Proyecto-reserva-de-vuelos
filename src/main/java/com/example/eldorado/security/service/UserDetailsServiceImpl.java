85a32f6ac140335f64289b2f2fd603b28e4e39f9
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-11-25 01:35:25 -0500

    methods of load and save in bd of  Users

--- src/main/java/com/example/eldorado/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/eldorado/security/service/UserDetailsServiceImpl.java
@@ -1,58 +1,52 @@
 package com.example.eldorado.security.service;
 
+import com.example.eldorado.dto.UserInfoDto;
 import com.example.eldorado.entity.ERole;
 import com.example.eldorado.entity.Role;
 import com.example.eldorado.entity.User;
-import com.example.eldorado.repository.RoleRepository;
 import com.example.eldorado.repository.UserRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final RoleRepository roleRepository;
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserDetailsServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+    private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByEmail(email);
 
-        return UserDetailsImp.build(user);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException("[ERROR]: is Empty");
+        }
+        return UserDetailsImp.build(userOptional.get());
     }
 
-    //Register
-
-    public ResponseEntity<String> singUp(Map<String, String> requestMap) {
-        return null;
-    }
-
-    private User getUserFromMap(Map<String, String> requestMap) {
-
-        User user = new User();
-        Role roleUser = roleRepository.findByName(ERole.ROLE_USER)
-                        .orElseThrow(() -> new RuntimeException("Error: Role not found"));
-
-        Set<Role> roles = new HashSet<>();
-        roles.add(roleUser);
-
-        user.setUsername(requestMap.get("username"));
-        user.setPassword(requestMap.get("password"));
-        user.setEmail(requestMap.get("email"));
-        user.setRoles(roles);
-
-        return user;
+    public UserInfoDto addUser(UserInfoDto userInfo) {
+        User user = new User(null, userInfo.name(), userInfo.email(), passwordEncoder.encode(userInfo.password()),
+                Arrays.stream(userInfo.roles().split(" "))
+                        .map(role -> new Role(ERole.valueOf(role)))
+                        .collect(Collectors.toSet())
+        );
+        user = userRepository.save(user);
+        return new UserInfoDto(user.getUsername(), user.getEmail(), userInfo.password(), userInfo.roles());
     }
 }
