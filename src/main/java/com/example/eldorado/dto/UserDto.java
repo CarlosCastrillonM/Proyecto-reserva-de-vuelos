675bdaa729bbab545052952e227bce5e320057f1
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-11-25 01:31:02 -0500

    new Class UserDto

---
+++ src/main/java/com/example/eldorado/dto/UserDto.java
@@ -0,0 +1,69 @@
+package com.example.eldorado.dto;
+
+import com.example.eldorado.entity.Role;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDto {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private Set<Role> roles = new HashSet<>();
+
+    public UserDto(Integer id, String username, String password, String email, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public UserDto(Integer id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
